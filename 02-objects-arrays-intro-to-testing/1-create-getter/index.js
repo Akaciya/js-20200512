0f4108e224688b0b5dbc6d4ff0e8eb00863861ee
Akaciya <start30@yandex.ru>
2020-05-21 02:16:38 +0300

    02-objects-arrays-intro-to-testing

--- 02-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 02-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,6 +4,7 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+
  /* 1 вариант
  return function(arg) {
     const arr = path.split('.');

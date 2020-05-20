dd980b2b3133e6891eb170a933514260b515f0be
Akaciya <start30@yandex.ru>
2020-05-21 01:56:59 +0300

    Задачи из второго задания

--- 02-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 02-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,27 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+ /* 1 вариант
+ return function(arg) {
+    const arr = path.split('.');
+    for (let item of arr) {
+      if (arg !== undefined && Object.keys(arg).indexOf(item) !== -1) {
+        arg = arg[item];
+      } else {
+        return;
+      }
+    }
+    return arg;
+  };*/
 
+  return function(arg) {
+    const arr = path.split('.');
+    for (let item of arr) {
+      if (arg === undefined || !arg.hasOwnProperty(item)) {
+        return;
+      }
+      arg=arg[item];
+    }
+    return arg;
+  };
 }
--- 02-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 02-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,25 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  if (size === undefined) {
+    return string;
+  }
+  if (size === 0) {
+    return '';
+  }
+  string = string.split('');
+  let count = 0;
+  for ( let i = 1; i < string.length; i++) {
+    if (string[i-1] === string[i]) {
+      count++;
+      if(count < size) {
+        continue;
+      }
+      string.splice(i, 1);
+      i--;
+    } else {
+      count = 0;
+    }
+  }
+  return string.join('');
 }

14cf632dbf835805cde9994be61f3d6f7f16a706
Akaciya <start30@yandex.ru>
2020-05-25 20:40:53 +0300

    03-oop-basic-intro-to-dom

--- 03-oop-basic-intro-to-dom/2-notification/src/index.js
+++ 03-oop-basic-intro-to-dom/2-notification/src/index.js
@@ -1,3 +1,59 @@
 export default class NotificationMessage {
+  element;
 
+  constructor(message, dataObj) {
+    this.message = message || 'Hello world';
+    this.type = dataObj ? dataObj.type : 'success';
+    this.duration = dataObj ? dataObj.duration : 2000;
+    this.render();
+  }
+
+  get template() {
+    return `
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">${this.type}</div>
+      <div class="notification-body">
+        ${this.message}
+      </div>
+    </div>
+    `
+  }
+
+  render() {
+    //this.type = Math.random() > 0.5 ? 'success' : 'error';
+    this.switcher();
+    this.element = document.createElement('div');
+    this.element.classList.add('notification', this.typeClass);
+    this.element.style.cssText = `--value:${this.duration/1000}s`;
+    this.element.innerHTML = this.template;
+  }
+   show(div) {
+    if (NotificationMessage.linkElement) {
+      NotificationMessage.linkElement.remove();
+      NotificationMessage.linkElement = null;//не уверена, надо ли оно тут. А вдруг?
+    }
+    div ? div.append(this.element) : document.body.append(this.element);
+    NotificationMessage.linkElement = this;
+    setTimeout(() => this.remove(), this.duration);
+  }
+  switcher() {
+    if(this.type === 'error') {
+      this.typeClass = 'error';
+     // this.message = 'Что-то пошло не так :('
+    } else {
+      this.typeClass = 'success';
+     // this.message = 'Данные получены!'
+    }
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.element.remove();
+  }
 }
+
+NotificationMessage.linkElement = null;
